41ff90d6ad785d1ecfbedb7688f14111b6e5df5d
Tyson <65432197+TYSON-Alii@users.noreply.github.com>
2020-11-09 18:18:07 +0300

    Add files via upload

---
+++ MORSalpfabet.cpp
@@ -0,0 +1,468 @@
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    int q = 300;
+    int z = 150;
+    int x = 1900;
+    int y = 700;
+    string k1;
+
+    tekrar:
+    cin >> k1;
+
+  while(TRUE)
+    {
+
+    if(k1 == "a" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "b" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "c" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "d" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "e" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "f" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "g" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "h" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "i" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "j")
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "k" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "l" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "m" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "n" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "o" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "p" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "r" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "s" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "t" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "u" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+
+    else if(k1 == "v" )
+    {
+    cout << endl;
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    else if(k1 == "y" )
+    {
+    cout << endl;
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 4a");
+    Sleep(y);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color a4");
+    Sleep(x);
+    system("color f0");
+    Sleep(z);
+    system("color 1f");
+    Sleep(q);
+    system("cls");
+    goto tekrar;
+    }
+    }
+    return 0;
+}
